6e60d04892ccc97130ba3dba3a48080d9ec0d213
a.23308051280301 <a.23308051280301@cbtis128.edu.mx>
2026-02-12 20:04:36 +0000

    Act8 listview Ortega 0301

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,106 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppEventos());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppEventos extends StatelessWidget {
+  const AppEventos({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Eventos Poplas",
+      home: const EventosOrganizados(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class EventosOrganizados extends StatelessWidget {
+  const EventosOrganizados({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text(
+          '"Eventos Woody" Paola Ortega 6J',
+          style: TextStyle(color: Colors.black),
         ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 107, 218, 255), // Azul clarito (LightBlue)
+        leading: const Icon(
+          Icons.cake, // Icono de pastel
+          color: Color.fromARGB(255, 10, 75, 129),
+        ),
+        actions: const [
+          Icon(
+            Icons.camera, // Icono de globo
+            color: Color.fromARGB(255, 10, 75, 129),
+          ),
+          SizedBox(width: 15),
+          Icon(
+            Icons.calendar_month, // Icono de calendario
+            color: Color.fromARGB(255, 10, 75, 129),
+          ),
+          SizedBox(width: 15),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+body: ListView.builder(
+  padding: const EdgeInsets.all(16.0),
+  itemCount: 4, // Definimos los 4 elementos
+  itemBuilder: (context, index) {
+    // Definimos una lista de datos para que cada elemento sea único
+    List<Map<String, dynamic>> items = [
+      {'titulo': 'Baby Shower de Lucia', 'sub': 'Proximo nacimiento del bebé de Lucia', 'icon': Icons.person, 'color': Colors.blueAccent},
+      {'titulo': 'Fiesta de cumpleaños de Jesus', 'sub': 'Cumpleaños #18 de Jesus', 'icon': Icons.wallet, 'color': Colors.greenAccent[700]},
+      {'titulo': 'Aniversario de Ambar', 'sub': 'Festejo del aniversario #6 de casados', 'icon': Icons.lock, 'color': Colors.orangeAccent},
+      {'titulo': 'Fiesta familiar Terrazas', 'sub': 'Convivio familiar', 'icon': Icons.support_agent, 'color': Colors.redAccent},
+    ];
+
+    final item = items[index];
+
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.only(bottom: 15),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        // LEADING: Icono con fondo circular moderno
+        leading: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: item['color'].withOpacity(0.15),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(item['icon'], color: item['color']),
+        ),
+        // TÍTULO Y SUBTÍTULO
+        title: Text(
+          item['titulo'],
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+        ),
+        subtitle: Text(item['sub']),
+        // TRAILING: Botón de acción funcional
+        trailing: const Icon(Icons.chevron_right, color: Colors.grey),
+        // FUNCIONALIDAD: Al presionar muestra un mensaje
+        onTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Abriendo: ${item['titulo']}'),
+              backgroundColor: item['color'],
+              behavior: SnackBarBehavior.floating,
+              duration: const Duration(seconds: 1),
+            ),
+          );
+        },
+      ),
+    );
+  },
+),
+
     );
   }
 }
